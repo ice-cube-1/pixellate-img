1215e25d6bede9015ca73946fc5ff25c0e9af9b3
ice-cube-1 <alice.goward@spgs.org>
2023-05-16 20:54:35 +0100

    done some stuff

--- improved.py
+++ improved.py
@@ -1,23 +1,25 @@
 import numpy as np
 from PIL import Image
-
-length = 64
 im = Image.open('idkthing.png')
 l=im.load()
-sizeorig=im.size[0]
+origl=im.size[0]
+origw=im.size[1]
+lgth=32
+w=int(round((origw/origl)*lgth,0))
+print(w,lgth)
 lsmallrow=[]
-for i in range(length):
+for i in range(lgth):#
     toaplsr=[]
-    for j in range(length):
+    for j in range(w):
         toaptr,toaptg,toaptb=0,0,0
-        for k in range(j*(sizeorig//length),(j*(sizeorig//length))+(sizeorig//length)):
-            for m in range(i*(sizeorig//length),(i*(sizeorig//length))+(sizeorig//length)):
+        for k in range(j*(origw//w),(j*(origw//w))+(origw//w)):
+            for m in range(i*(origl//lgth),(i*(origl//lgth))+(origl//lgth)):
                 toaptr+=l[m,k][0]
                 toaptg+=l[m,k][1]
                 toaptb+=l[m,k][2]
-        toaptr=int(round(toaptr/(sizeorig//length)**2,0))       
-        toaptg=int(round(toaptg/(sizeorig//length)**2,0))
-        toaptb=int(round(toaptb/(sizeorig//length)**2,0))
+        toaptr=int(round(toaptr/(origl*origw/(lgth*w)),0))       
+        toaptg=int(round(toaptg/(origl*origw/(lgth*w)),0))
+        toaptb=int(round(toaptb/(origl*origw/(lgth*w)),0))
         toaplsr.append((toaptr,toaptg,toaptb))
     lsmallrow.append(toaplsr)
 array = np.array(lsmallrow, dtype=np.uint8)
