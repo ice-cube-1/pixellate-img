074eb87291e168e3e67f0869d420b3a73224277d
ice-cube-1 <alice.goward@spgs.org>
2023-05-17 14:23:26 +0100

    line 9 col 248

--- stuff.py
+++ stuff.py
@@ -1,20 +1,9 @@
 import numpy as np
 from PIL import Image
-
-def pixellate(im,lnew):
-    img=im.load()
-    lold,wold=im.size[0],im.size[1]
+def pixellate(im,lnew,noc=256):
+    lold,wold,img=im.size[0],im.size[1],im.load()
     wnew=int(round((wold/lold)*lnew,0))
     ratio = wold//wnew
-    woffset=int(round((wold%wnew)/2,0))
-    loffset=int(round((lold%lnew)/2,0))
-    return [[tuple([sum(img[k,l][m] for k in range(i*ratio+loffset,i*ratio+ratio+loffset) for l in range(j*ratio+woffset,j*ratio+ratio+woffset))//(ratio**2) for m in range(3)]) for j in range(wnew)] for i in range(lnew)]
-
-def make8bit(vals,noc):
-    return [([tuple([round(k*noc/256,0)*(256/noc) for k in j]) for j in i]) for i in vals]
-    
-image = Image.open('door.jpg')
-length=16
-pxls=make8bit(pixellate(image,length),32)
-pxli = Image.fromarray(np.array(pxls, dtype=np.uint8).swapaxes(0,1))
-pxli.save('output.png')
+    woffset,loffset=int(round((wold%wnew)/2,0)),int(round((lold%lnew)/2,0))
+    return [[tuple([round(sum(img[k,l][m] for k in range(i*ratio+loffset,i*ratio+ratio+loffset) for l in range(j*ratio+woffset,j*ratio+ratio+woffset))//(ratio**2)*(noc/256),0)*256/noc for m in range(3)]) for j in range(wnew)] for i in range(lnew)]
+Image.fromarray(np.array(pixellate(Image.open('door.jpg'),16,32), dtype=np.uint8).swapaxes(0,1)).save('output.png')
