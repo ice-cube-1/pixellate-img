9ac9fbe25d33afdb48f4e075d85de975e33b5d1a
ice-cube-1 <alice.goward@spgs.org>
2023-05-17 13:26:36 +0100

    questionable optimization

--- stuff.py
+++ stuff.py
@@ -6,19 +6,9 @@ def pixellate(im,lnew):
     lold,wold=im.size[0],im.size[1]
     wnew=int(round((wold/lold)*lnew,0))
     ratio = wold//wnew
-    woffset=round((wold%wnew)/2,0)
-    loffset=round((lold%lnew)/2,0)
-    outvals=[]
-    for i in range(lnew):
-        rowpix=[]
-        for j in range(wnew):
-            toapt=[0,0,0]
-            for k in range(j*(ratio),(j*(ratio))+(ratio)): 
-                for l in range(i*(ratio),(i*(ratio))+(ratio)):
-                    toapt=[toapt[i]+img[l+loffset,k+woffset][i] for i in range(3)]
-            rowpix.append(tuple([int(round(toapt[i]/ratio**2,0)) for i in range(3)]))
-        outvals.append(rowpix)
-    return outvals
+    woffset=int(round((wold%wnew)/2,0))
+    loffset=int(round((lold%lnew)/2,0))
+    return [[tuple([sum(img[k,l][m] for k in range(i*ratio+loffset,i*ratio+ratio+loffset) for l in range(j*ratio+woffset,j*ratio+ratio+woffset))//(ratio**2) for m in range(3)]) for j in range(wnew)] for i in range(lnew)]
 
 def make8bit(vals,noc):
     return [([tuple([round(k*noc/256,0)*(256/noc) for k in j]) for j in i]) for i in vals]
